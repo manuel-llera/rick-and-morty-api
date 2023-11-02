ca30ebac108c242c92d92830580b1d113a014a32
manuel-llera <62348319+manuel-llera@users.noreply.github.com>
2023-11-02 11:30:56 +0100

    Update RickAndMortyApiApplicationTests.java

--- src/test/java/com/rickandmorty/restapi/RickAndMortyApiApplicationTests.java
+++ src/test/java/com/rickandmorty/restapi/RickAndMortyApiApplicationTests.java
@@ -5,37 +5,57 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
 import com.rickandmorty.restapi.controller.RickAndMortyController;
 import com.rickandmorty.restapi.service.RickAndMortyService;
 
-@SpringBootTest
-@ContextConfiguration(classes=RickAndMortyApiApplication.class)
 @WebMvcTest(RickAndMortyController.class)
-class RickAndMortyApiApplicationTests {
+class RickAndMortyApiControllerTests {
 	
     private static final String END_POINT_WITHOUT_PARAMS = "/search-character-appearance/";
+    private static final String END_POINT_WITH_PARAM = "/search-character-appearance/?name=";
+    private static final String XXXXX_PARAM_NAME = "xxxxx";
  
     @Autowired
     private MockMvc mockMvc;
     
     @MockBean
     private RickAndMortyService rickAndMortyService;
- 
+    
+    @MockBean
+    WebClient webClient;
+    
+    @Test
+    void whenIsNotSetPathParamNameVariableReturn400BadRequest() throws Exception {
+        mockMvc.perform(get(END_POINT_WITHOUT_PARAMS).contentType("application/json"))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+    
+    @Test
+    void whenPathParamNameIsEmptyReturn400BadRequest() throws Exception {
+        mockMvc.perform(get(END_POINT_WITH_PARAM).contentType("application/json"))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+    
 //    @Test
-//    void testWhenIsNotSetPathParamNameVariableReturn400BadRequest() throws Exception {
+//    void whenCharacterIsNotFoundExactlyReturn404IsNotFound() throws Exception {
 //    	
-//        mockMvc.perform(get(END_POINT_WITHOUT_PARAMS).contentType("application/json"))
-//                .andExpect(status().isBadRequest())
+//    	Mockito.when(rickAndMortyService.getCharacterInfo(XXXXX_PARAM_NAME)).thenThrow(WebClientResponseException.class);
+//    	Mockito.when(webClient.get()).thenThrow(WebClientResponseException.class);
+//    	
+//        mockMvc.perform(get(END_POINT_WITH_PARAM + XXXXX_PARAM_NAME).contentType("application/json"))
+//                .andExpect(status().isNotFound())
 //                .andDo(print());
 //        
-//        
 //    }
- 
+    
 }
