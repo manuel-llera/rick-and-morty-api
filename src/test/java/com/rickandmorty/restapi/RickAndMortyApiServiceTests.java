cfee6b456b68058daae350301f7d4b38ca4e4e9d
manuel-llera <62348319+manuel-llera@users.noreply.github.com>
2023-11-02 11:31:50 +0100

    Add files via upload

---
+++ src/test/java/com/rickandmorty/restapi/RickAndMortyApiServiceTests.java
@@ -0,0 +1,13 @@
+package com.rickandmorty.restapi;
+
+import org.junit.jupiter.api.Test;
+
+class RickAndMortyApiServiceTests {
+	
+    @Test
+    void whenIsNotSetPathParamNameVariableReturn400BadRequest() throws Exception {
+    	// empty method
+    }
+    
+    
+}
